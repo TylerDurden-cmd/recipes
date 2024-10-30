189908ae2ba5dc1b5658b27d1ae0331dcb672db7
TylerDurden-cmd <125083855+TylerDurden-cmd@users.noreply.github.com>
2024-10-30 13:55:12 +0000

    scroll tru menu

--- Project/recipes/App.tsx
+++ Project/recipes/App.tsx
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image, Text, ScrollView } from 'react-native';
 import { IRecipes } from './IRecipes';
+import Constants from 'expo-constants';
 
 interface IData {
-  GetData:React.Dispatch<React.SetStateAction<IRecipes[]>>
-  data:IRecipes[]
+  GetData: React.Dispatch<React.SetStateAction<IRecipes[]>>
+  data: IRecipes[]
 }
 
-const Home = ({data}:IData) => {
+const Home = ({ data }: IData) => {
 
   const FilterData = data.filter((item, index, self) =>
     index === self.findIndex((t) => t.cuisine === item.cuisine) && item.cuisine != ""
@@ -18,24 +18,47 @@ const Home = ({data}:IData) => {
   /* shows all the data that doesnt have cuisine doubles */
   console.log(FilterData)
 
+  const CuisineImage = (cuisine: string) => {
+    switch (cuisine) {
+      case 'Italian':
+        return require('./assets/Italian.png');
+      case 'Asian':
+        return require('./assets/Asian.png');
+      case 'American':
+        return require('./assets/American.png');
+      case 'Mexican':
+        return require('./assets/Mexican.png');
+
+      default:
+        return require('./assets/icon.png')
+    }
+  }
+
 
   return (
-    <View style={styles.cuisine} >
-      {FilterData.map((data) => 
-      <Text key={data.id}>
-        {data.cuisine}
-      </Text>
-      )}
+    <View>
+      <ScrollView>
+        {FilterData.map((data) =>
+          <View key={data.id} style={styles.cuisine}>
+            <Image source={CuisineImage(data.cuisine)} style={styles.imgCuisine} />
+
+            <Text>
+              {data.cuisine}
+            </Text>
+
+          </View>
+        )}
+      </ScrollView>
     </View>
   )
 }
 
 const Root = () => {
-  const [data,GetData] = React.useState<IRecipes[]>([])
+  const [data, GetData] = React.useState<IRecipes[]>([])
 
   useEffect(() => {
-    const ApiFetch = async() => {
-      const uri:IRecipes[] = await fetch("https://sampleapis.assimilate.be/recipes/recipes").then((prom) => prom.json());
+    const ApiFetch = async () => {
+      const uri: IRecipes[] = await fetch("https://sampleapis.assimilate.be/recipes/recipes").then((prom) => prom.json());
       GetData(uri);
     }
     ApiFetch()
@@ -43,7 +66,7 @@ const Root = () => {
 
   return (
     <View>
-      <Home data={data} GetData={GetData}/>
+      <Home data={data} GetData={GetData} />
     </View>
   )
 }
@@ -52,8 +75,8 @@ const Root = () => {
 export default function App() {
 
   return (
-    <View style={styles.container}> 
-    <Root/>
+    <View style={styles.container}>
+      <Root />
       <StatusBar style="auto" />
     </View>
   );
@@ -62,13 +85,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    alignContent: 'center',
+    paddingTop: Constants.statusBarHeight,
   },
-  cuisine:{
-    width: 100,
-    height:100,
-    backgroundColor:'white',
+  cuisine: {
+    backgroundColor: '#fff',
+    flex: 1,
+    alignItems: 'center',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#000',
+    width: 370,
+    minHeight: 250,
+    borderRadius: 25,
+    maxHeight: 300,
+    marginBottom: 25
+  },
+  imgCuisine: {
+    width: 370,
+    height: 150,
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    marginBottom: 20,
   }
 });
