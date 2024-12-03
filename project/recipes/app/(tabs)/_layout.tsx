9899ade1e355f20a37934f9b81cae5d3b426e1ab
TylerDurden-cmd <125083855+TylerDurden-cmd@users.noreply.github.com>
2024-12-03 22:14:08 +0000

    camera

--- project/recipes/app/(tabs)/_layout.tsx
+++ project/recipes/app/(tabs)/_layout.tsx
@@ -1,11 +1,24 @@
 import { Tabs } from "expo-router"
 import React from "react"
+import { FontAwesome } from "@expo/vector-icons";
 
 const TabsLayout = () => {
     return (
         <Tabs>
-            <Tabs.Screen name="home" options={{ headerShown: false }} />
-            <Tabs.Screen name="camera" options={{ headerShown: false }} />
+            <Tabs.Screen
+                name="home"
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => <FontAwesome name="home" size={size} color={color} />
+                }}
+            />
+            <Tabs.Screen
+                name="camera"
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => <FontAwesome name="camera" size={size} color={color} />
+                }}
+            />
         </Tabs>
     )
 }
--- project/recipes/app/(tabs)/camera.tsx
+++ project/recipes/app/(tabs)/camera.tsx
@@ -1,12 +1,100 @@
-import React from "react";
-import { View } from "react-native"
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import React, { useRef } from 'react';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FontAwesome } from "@expo/vector-icons";
+import PhotoPreviewSection from '@/components/PhotoPreviewSection';
 
-const Camera = () => {
+export default function CameraComponent() {
+    const [facing, setFacing] = useState<CameraType>('back');
+    const [permission, requestPermission] = useCameraPermissions();
+    const [photo, SetPhoto] = useState<any>(null);
+    const cameraRef = useRef<CameraView | null>(null);
+
+
+    if (!permission) {
+        // Camera permissions are still loading.
+        return <View />;
+    }
+
+
+    if (!permission.granted) {
+        // Camera permissions are not granted yet.
+        return (
+            <View style={styles.container}>
+                <Text style={styles.message}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+
+    function toggleCameraFacing() {
+        setFacing(current => (current === 'back' ? 'front' : 'back'));
+    }
+
+    const handleTakePhoto = async () => {
+        if (cameraRef.current) {
+            const options = {
+                quality: 1,
+                base64: true,
+                exif: false
+            };
+
+            const takedPhoto = await cameraRef.current.takePictureAsync(options);
+
+            SetPhoto(takedPhoto)
+        }
+
+    };
+
+    const handleRetakePhoto = () => SetPhoto(null);
+
+    if (photo) {
+        return <PhotoPreviewSection photo={photo} handleRetakePhoto={handleRetakePhoto} />
+    }
+
     return (
-        <View>
-
+        <View style={styles.container}>
+            <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+                        <FontAwesome name='retweet' size={40} color={"black"} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={handleTakePhoto}>
+                        <FontAwesome name='camera' size={40} color={"black"} />
+                    </TouchableOpacity>
+                </View>
+            </CameraView>
         </View>
-    )
+    );
 }
 
-export default Camera;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    message: {
+        textAlign: 'center',
+        paddingBottom: 10,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
